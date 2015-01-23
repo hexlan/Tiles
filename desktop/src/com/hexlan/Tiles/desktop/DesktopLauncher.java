66ce3f9cd185b6188b17a1c6f5a6780d5ecbcb9b
Hexlan <rswengel@css.edu>
2015-01-22 19:58:41 -0600

    Adding Libgdx

---
+++ desktop/src/com/hexlan/Tiles/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hexlan.Tiles.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hexlan.Tiles.Tiles;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Tiles(), config);
+	}
+}
